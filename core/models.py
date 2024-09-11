5336e9c9897895ff2ac5fd7c9f1aa3809dddad67
Mb3r3 <essienmbereidem@gmail.com>
2024-09-11 01:57:33 +0100

    token table enabled

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.forms import DateTimeField
 
 # Create your models here.
+
+
+class Token(models.Model):
+    user = models.CharField(unique=True, max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    access_token = models.CharField(max_length=500)
+    refresh_token = models.CharField(max_length=500)
+    expires_in = models.DateTimeField()
+    token_type = models.CharField(max_length=50)
